0c0c6d93979dfa50e0d6c830b78a0d30ff4f7b3d
PierpaoloIannone <piannone@byte-code.com>
2017-09-03 17:05:21 +0200

    feat: implemented api call to get all books

--- src/components/BookShelfList.js
+++ src/components/BookShelfList.js
@@ -1,19 +1,35 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import homeDecorator from "./hoc/homeDecorator";
 import BookShelf from "./BookShelf";
+import * as BooksAPI from "../BooksAPI.js";
 
 class BookShelfList extends Component {
   state = {
-    books: PropTypes.array.isRequired
+    books: []
   };
+
+  componentDidMount() {
+    BooksAPI.getAll().then(response => {
+      console.log(response);
+      this.setState({ books: response });
+    });
+  }
+
   render() {
+    const curReading = this.state.books.filter(
+      book => book.shelf === "currentlyReading"
+    );
+    const wantRead = this.state.books.filter(
+      book => book.shelf === "wantToRead"
+    );
+    const read = this.state.books.filter(book => book.shelf === "read");
+
     return (
       <div className="list-books-content">
         <div>
-          <BookShelf title={"Currently Reading"} />
-          <BookShelf title={"Want to Read"} />
-          <BookShelf title={"Read"} />
+          <BookShelf books={curReading} title={"Currently Reading"} />
+          <BookShelf books={wantRead} title={"Want to Read"} />
+          <BookShelf books={read} title={"Read"} />
         </div>
       </div>
     );
