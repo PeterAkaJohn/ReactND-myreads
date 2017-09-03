dc5fd3fa1862074abfd6e8bda468de5a254f4e1c
PierpaoloIannone <piannone@byte-code.com>
2017-09-03 17:05:56 +0200

    feat: reused BookList component, search api call

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import * as BooksAPI from "../BooksAPI";
+import BookList from "./BookList";
 
 class SearchComponent extends Component {
-    render() {
-        return (
-            <div className="search-books">
-            <div className="search-books-bar">
-              <Link to='/' className="close-search">Close</Link>
-              <div className="search-books-input-wrapper">
-                {/*
+  state = {
+    books: [],
+    query: ""
+  };
+  onUpdateQuery = event => {
+    this.setState({ query: event.target.value });
+    BooksAPI.search(event.target.value, 20).then(books => {
+      this.setState({ books });
+      console.log(books);
+    });
+  };
+  render() {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link to="/" className="close-search">
+            Close
+          </Link>
+          <div className="search-books-input-wrapper">
+            {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
@@ -16,16 +31,20 @@ class SearchComponent extends Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
+            <input
+              type="text"
+              onChange={this.onUpdateQuery}
+              value={this.state.query}
+              placeholder="Search by title or author"
+            />
           </div>
-        )
-    }
+        </div>
+        <div className="search-books-results">
+          <BookList books={this.state.books} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SearchComponent;
