6eddac4f1025adc3d41bc7f8d91380d926ad3575
PierpaoloIannone <piannone@byte-code.com>
2017-09-09 12:47:09 +0200

    feat: added state and logic function to BooksApp
    BooksApp will now handle the user favorites books along with fetching and refreshing said data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
-import React from 'react'
+import React from "react";
 // import * as BooksAPI from './BooksAPI'
-import './App.css'
-import { BrowserRouter, Route } from 'react-router-dom';
-import BookShelfList from './components/BookShelfList';
-import SearchComponent from './components/SearchComponent';
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import BookShelves from "./components/BookShelves";
+import SearchComponent from "./components/SearchComponent";
+import * as BooksAPI from "./BooksAPI.js";
 
 class BooksApp extends React.Component {
+  state = {
+    userBooks: []
+  };
+
+  componentDidMount() {
+    this.getAllBooks();
+  }
+
+  getAllBooks = () => {
+    BooksAPI.getAll().then(userBooks => {
+      this.setState({ userBooks });
+    });
+  };
+
+  refreshBooks = () => {
+    this.getAllBooks();
+  };
+
   render() {
     return (
       <BrowserRouter>
         <div className="app">
-          <Route exact path='/' render={BookShelfList} />
-          <Route path='/search' component={SearchComponent} />
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <BookShelves
+                userBooks={this.state.userBooks}
+                refreshBooks={this.refreshBooks}
+              />
+            )}
+          />
+          <Route
+            path="/search"
+            render={() => (
+              <SearchComponent
+                userBooks={this.state.userBooks}
+                refreshBooks={this.refreshBooks}
+              />
+            )}
+          />
         </div>
       </BrowserRouter>
-    )
+    );
   }
 }
 
-export default BooksApp
+export default BooksApp;
