f7fea6401a307e4af47f604fc1aaa53073634852
PierpaoloIannone <piannone@byte-code.com>
2017-09-09 12:47:49 +0200

    feat: Book component is now handling update

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,38 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
+import * as BooksAPI from "../BooksAPI";
 
-function Book(props) {
-  return (
-    <li>
-      <div className="book">
-        <div className="book-top">
-          <div
-            className="book-cover"
-            style={{
-              width: 128,
-              height: 193,
-              backgroundImage: `url(${props.book.imageLinks.thumbnail})`
-            }}
-          />
-          <div className="book-shelf-changer">
-            <select defaultValue="none" value={props.book.shelf}>
-              <option value="none" disabled>
-                Move to...
-              </option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
+class Book extends Component {
+  updateBook = event => {
+    event.preventDefault();
+    this.props.book.shelf = event.target.value;
+    BooksAPI.update(this.props.book, event.target.value).then(response => {
+      console.log(response);
+      this.props.refreshBooks();
+    });
+  };
+  render() {
+    return (
+      <li>
+        <div className="book">
+          <div className="book-top">
+            <div
+              className="book-cover"
+              style={{
+                width: 128,
+                height: 193,
+                backgroundImage: `url(${this.props.book.imageLinks.thumbnail})`
+              }}
+            />
+            <div className="book-shelf-changer">
+              <select
+                onChange={this.updateBook}
+                value={this.props.book.shelf || "none"}
+              >
+                <option value="disabled" disabled>
+                  Move to...
+                </option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+          </div>
+          <div className="book-title">{this.props.book.title}</div>
+          <div className="book-authors">
+            {this.props.book.authors &&
+              this.props.book.authors.map(author => (
+                <span key={author}>{author}</span>
+              ))}
           </div>
         </div>
-        <div className="book-title">{props.book.title}</div>
-        <div className="book-authors">
-          {props.book.authors && props.book.authors.map(author => <span key={author}>{author}</span>)}
-        </div>
-      </div>
-    </li>
-  );
+      </li>
+    );
+  }
 }
 
 Book.propTypes = {
