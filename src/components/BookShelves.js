14a6450b3dd48c2fe2f2a905605e380d05a64fde
PierpaoloIannone <piannone@byte-code.com>
2017-09-10 12:14:11 +0200

    feat: Iterate over array of shelfs, use withBooks hoc

--- src/components/BookShelves.js
+++ src/components/BookShelves.js
@@ -1,42 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import homeDecorator from './hoc/homeDecorator';
+import withBooks from './hoc/withBooks';
 import BookShelf from './BookShelf';
 
-function mapBooksObject(books) {
-  return {
-    currentlyReading: {
+function mapBooksObjectToArray(books) {
+  return [
+    {
       title: 'Currently Reading',
       books: books.filter(book => book.shelf === 'currentlyReading'),
     },
-    wantToRead: {
+
+    {
       title: 'Want To Read',
       books: books.filter(book => book.shelf === 'wantToRead'),
     },
-    read: {
+
+    {
       title: 'Read',
       books: books.filter(book => book.shelf === 'read'),
     },
-  };
+  ];
 }
 
 function BookShelves(props) {
-  const { currentlyReading, wantToRead, read } = mapBooksObject(props.userBooks);
+  const shelves = mapBooksObjectToArray(props.userBooks);
 
   return (
     <div className="list-books-content">
       <div>
-        <BookShelf
-          books={currentlyReading.books}
-          title={currentlyReading.title}
-          refreshBooks={props.refreshBooks}
-        />
-        <BookShelf
-          books={wantToRead.books}
-          title={wantToRead.title}
-          refreshBooks={props.refreshBooks}
-        />
-        <BookShelf books={read.books} title={'Read'} refreshBooks={props.refreshBooks} />
+        {shelves.map(shelf => (
+          <BookShelf
+            key={shelf.title}
+            books={shelf.books}
+            title={shelf.title}
+            refreshBooks={props.refreshBooks}
+          />
+        ))}
       </div>
     </div>
   );
@@ -47,4 +47,4 @@ BookShelves.propTypes = {
   refreshBooks: PropTypes.func.isRequired,
 };
 
-export default homeDecorator(BookShelves);
+export default withBooks(homeDecorator(BookShelves));
