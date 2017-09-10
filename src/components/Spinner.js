53fc03a5725f0524d3a6d675c2ebc2c08c71660c
PierpaoloIannone <piannone@byte-code.com>
2017-09-10 12:12:56 +0200

    feat: Add spinner component and hoc to use it

---
+++ src/components/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MDSpinner from 'react-md-spinner';
+
+function Spinner() {
+  return (
+    <div className="full-screen">
+      <MDSpinner size="80" />
+    </div>
+  );
+}
+
+export default Spinner;
---
+++ src/components/hoc/withBooks.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Spinner from '../Spinner';
+
+export default function withBooks(Component) {
+  function WithBooks(props) {
+    if (props.userBooks.length > 0 || props.books.length > 0) {
+      return <Component {...props} />;
+    }
+    return <Spinner />;
+  }
+  WithBooks.defaultProps = {
+    userBooks: [],
+    books: [],
+  };
+  return WithBooks;
+}
